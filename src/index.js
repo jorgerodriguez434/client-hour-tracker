33b01203bafa5950e02a7e3c65e56d9c997deb5d
Jorge R <jorgerodriguez434@gmail.com>
2018-08-30 13:37:55 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => (
+
+    <div> 
+
+        <h1> hello world
+             
+        </h1>
+    </div>
+    
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
