a0ba4f2cacf8e11f00aa429b527c4fced8112edf
Jorge R <jorgerodriguez434@gmail.com>
2018-09-09 06:48:45 -0500

    add config

--- src/components/form.js
+++ src/components/form.js
@@ -3,6 +3,7 @@ import "./form.css";
 import { connect } from "react-redux";
 import * as actions from "../actions";
 import Clients from "./clients";
+import { API_BASE_URL } from "../config";
 
 export class Form extends React.Component {
   constructor() {
@@ -10,26 +11,65 @@ export class Form extends React.Component {
     this._name = React.createRef();
     this._lastName = React.createRef();
   }
-  onSubmit(e) {
+  onSubmit = e =>  {
     e.preventDefault();
     console.log(`submit button has been clicked!`);
     const name = this._name.current.value;
     const lastName = this._lastName.current.value;
     this.props.dispatch(actions.setName(name));
     this.props.dispatch(actions.setLastName(lastName));
+
     const client = {
       name,
       lastName,
       hours: 0
     };
+
     this.props.dispatch(actions.addClient(client));
+    this._name.current.value = "";
+    this._lastName.current.value = "";
+    this.postRequestPromise();
+   
+  }
+
+  postRequest = () => {
+
+    const data = {
+      firstName: this.props.state.name,
+      lastName: this.props.state.lastName
+    }
+  
+    fetch(API_BASE_URL, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+        //Authorization: `Bearer ${localStorage.getItem("token")}`
+      },
+    })
+      .then(res => res.json())
+      .catch(error => console.error("Error:", error))
+      .then(response => console.log("Success:", response));
+  };
+
+  postRequestPromise = () => {
+
+    const wait = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve();
+      }, 1500);
+    });
+
+    wait.then(this.postRequest);
+
+
   }
 
   render() {
     console.log(this.props);
     return (
       <div>
-        <form onSubmit={this.onSubmit.bind(this)}>
+        <form onSubmit={this.onSubmit}>
           <h1> Register a client </h1>
           <label htmlFor="client-name">Client Name</label>
           <input ref={this._name} required />
