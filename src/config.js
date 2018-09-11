e75cda59731a517d225f60a942fc5f2bf81728af
Jorge R <jorgerodriguez434@gmail.com>
2018-09-11 13:16:02 -0500

    add heroku route

--- src/config.js
+++ src/config.js
@@ -1,2 +1,27 @@
 exports.API_BASE_URL =  process.env.REACT_APP_API_BASE_URL ||
-"http://localhost:8080/api/clients";
+//"http://localhost:8080/api/clients";
+"https://client-hour-tracker-api.herokuapp.com/api/clients";
+
+
+
+
+/**
+ * exports.API_BASE_URL =  process.env.REACT_APP_API_BASE_URL ||
+//"http://localhost:8080/api/dishes";
+'https://thawing-ravine-79238.herokuapp.com/api/dishes';
+exports.API_USERS_URL =  process.env.API_USERS_URL ||
+//"http://localhost:8080/api/users";
+'https://thawing-ravine-79238.herokuapp.com/api/users';
+exports.API_AUTH_LOGIN_URL =  process.env.API_AUTH_LOGIN_URL ||
+//"http://localhost:8080/api/auth/login";
+'https://thawing-ravine-79238.herokuapp.com/api/auth/login';
+exports.API_PROTECTED_URL =  process.env.API_PROTECTED_URL||
+//"http://localhost:8080/api/protected";
+'https://thawing-ravine-79238.herokuapp.com/api/protected';
+
+
+
+
+https://client-hour-tracker.herokuapp.com/
+https://client-hour-tracker-api.herokuapp.com/
+ */
